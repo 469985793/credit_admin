12b263b974ceb37b821cc949d5361af5439ecee3
zhulinbin <641361968@qq.com>
2018-05-14 23:16:56 +0800

    修改控制面板

--- src/pages/dashboard/Index.vue
+++ src/pages/dashboard/Index.vue
@@ -1,47 +1,14 @@
 <template>
   <div class="v_dashboard_container">
-    <el-row class="panel-group" :gutter="40">
-      <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-        <div class='card-panel'>
-          <div class="card-panel-icon-wrapper icon-people">
-            <span class="el-icon-warning"></span>
+    <el-row class="row_wrapper" :gutter="40">
+      <el-col class="card_box" :xs="12" :sm="12" :lg="6" v-for="(item, index) in cardList" :key="index">
+        <div class='item'>
+          <div class="icon_box">
+            <span :class="item.icon"></span>
           </div>
-          <div class="card-panel-description">
-            <div class="card-panel-text">放款金额</div>
-            <div class="card-panel-num">1233</div>
-          </div>
-        </div>
-      </el-col>
-      <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-        <div class='card-panel'>
-          <div class="card-panel-icon-wrapper icon-people">
-            <span class="el-icon-warning"></span>
-          </div>
-          <div class="card-panel-description">
-            <div class="card-panel-text">结清金额</div>
-            <div class="card-panel-num">1233</div>
-          </div>
-        </div>
-      </el-col>
-      <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-        <div class='card-panel'>
-          <div class="card-panel-icon-wrapper icon-people">
-            <span class="el-icon-warning"></span>
-          </div>
-          <div class="card-panel-description">
-            <div class="card-panel-text">逾期金额</div>
-            <div class="card-panel-num">1233</div>
-          </div>
-        </div>
-      </el-col>
-      <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-        <div class='card-panel'>
-          <div class="card-panel-icon-wrapper icon-people">
-            <span class="el-icon-warning"></span>
-          </div>
-          <div class="card-panel-description">
-            <div class="card-panel-text">逾期率</div>
-            <div class="card-panel-num">1233</div>
+          <div class="text_box">
+            <div class="title">{{item.title}}</div>
+            <div class="value">{{item.value}}</div>
           </div>
         </div>
       </el-col>
@@ -62,7 +29,29 @@ export default {
       lineChartData: {
         loanData: [100, 120, 161, 134, 105, 160, 165],
         overdueData: [120, 82, 91, 154, 162, 140, 145]
-      }
+      },
+      cardList: [
+        {
+          icon: 'el-icon-message',
+          title: '放款金额',
+          value: '111'
+        },
+        {
+          icon: 'el-icon-date',
+          title: '结清金额',
+          value: '111'
+        },
+        {
+          icon: 'el-icon-printer',
+          title: '逾期金额',
+          value: '111'
+        },
+        {
+          icon: 'el-icon-sold-out',
+          title: '逾期率',
+          value: '111'
+        }
+      ]
     }
   },
   components: { VLineChart },
@@ -74,74 +63,56 @@ export default {
 </script>
 
 <style lang="scss">
-.panel-group {
-  margin: 18px -12px 0 !important;
-  .card-panel-col{
-    margin-bottom: 32px;
-  }
-  .card-panel {
-    height: 108px;
-    cursor: pointer;
-    font-size: 12px;
-    position: relative;
-    overflow: hidden;
-    color: #666;
-    background: #fff;
-    box-shadow: 4px 4px 40px rgba(0, 0, 0, .05);
-    border-color: rgba(0, 0, 0, .05);
-    &:hover {
-      .card-panel-icon-wrapper {
-        color: #fff;
+@import '../../assets/css/vars.scss';
+
+.v_dashboard_container {
+  .row_wrapper {
+    margin: 25px -$ent-gap-x-small 0 !important;
+    .card_box {
+      margin-bottom: 32px;
+    }
+    .item {
+      height: 108px;
+      cursor: pointer;
+      font-size: 12px;
+      position: relative;
+      overflow: hidden;
+      color: #666;
+      background: #fff;
+      box-shadow: 4px 4px 40px rgba(0, 0, 0, .05);
+      border-color: rgba(0, 0, 0, .05);
+      &:hover {
+        .icon_box {
+          color: #fff;
+          background: #40c9c6;
+        }
       }
-      .icon-people {
-         background: #40c9c6;
+
+      .icon_box {
+        color: #40c9c6;
+        float: left;
+        margin: $ent-gap-small 0 0 $ent-gap-small;
+        padding: 16px;
+        transition: all 0.5s ease-out;
+        border-radius: 6px;
+        font-size: 40px;
       }
-      .icon-message {
-        background: #36a3f7;
-      }
-      .icon-money {
-        background: #f4516c;
-      }
-      .icon-shoppingCard {
-        background: #34bfa3
-      }
-    }
-    .icon-people {
-      color: #40c9c6;
-    }
-    .icon-message {
-      color: #36a3f7;
-    }
-    .icon-money {
-      color: #f4516c;
-    }
-    .icon-shoppingCard {
-      color: #34bfa3
-    }
-    .card-panel-icon-wrapper {
-      float: left;
-      margin: 14px 0 0 14px;
-      padding: 16px;
-      transition: all 0.38s ease-out;
-      border-radius: 6px;
-    }
-    .card-panel-icon {
-      float: left;
-      font-size: 48px;
-    }
-    .card-panel-description {
-      float: right;
-      font-weight: bold;
-      margin: 26px;
-      margin-left: 0px;
-      .card-panel-text {
-        line-height: 18px;
-        color: rgba(0, 0, 0, 0.45);
-        font-size: 16px;
-        margin-bottom: 12px;
-      }
-      .card-panel-num {
-        font-size: 20px;
+
+      .text_box {
+        float: right;
+        font-weight: bold;
+        margin: 26px;
+        margin-left: 0px;
+        .title {
+          line-height: 18px;
+          color: rgba(0, 0, 0, 0.45);
+          font-size: 14px;
+          margin-bottom: $ent-gap-small;
+        }
+        .value {
+          font-size: 20px;
+          color: $ent-color-danger;
+        }
       }
     }
   }
